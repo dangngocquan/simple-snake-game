bd31bc4307d39bccca1a3bd43c8200bb860718de
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2022-09-02 22:42:34 +0700

    Add Play Game Menu

--- code/game.py
+++ code/game.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 
-from mainMenu import MainMenu
+from menu import MainMenu, PlayGameMenu
 
 ###########  SCREEN SETTING  ####################################################
 SCREEN_WIDTH = 1000
@@ -22,10 +22,16 @@ class Game:
         self.countTicks = 0
         self.running = True
         self.waiting = False
+        
         self.runningMainMenu = True
+        self.runningPlayGameMenu = False
+        self.runningNewGameMenu = False
+        self.runningContinueGameMenu = False
+        self.runningOptionsMenu = False
         self.runningGame = False
         
         self.mainMenu = MainMenu(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.playGameMenu = PlayGameMenu(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, SCREEN_WIDTH, SCREEN_HEIGHT)
         
     def run(self):
         while self.running:
@@ -42,6 +48,10 @@ class Game:
                 self.running =False
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running =False
+                pygame.quit()
+                sys.exit()
             if self.runningMainMenu:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_DOWN or event.key == pygame.K_s:
@@ -50,14 +60,46 @@ class Game:
                     elif event.key == pygame.K_UP or event.key == pygame.K_w:
                         self.mainMenu.cursor -= 1
                         self.mainMenu.cursor %= 2
+                    elif event.key == pygame.K_RETURN:
+                        if self.mainMenu.cursor == 0:
+                            self.runningPlayGameMenu = True
+                        elif self.mainMenu.cursor == 1:
+                            self.runningOptionsMenu = True
+                        self.runningMainMenu = False
+            elif self.runningPlayGameMenu:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.playGameMenu.cursor += 1
+                        self.playGameMenu.cursor %= 3
+                    elif event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.playGameMenu.cursor -= 1
+                        self.playGameMenu.cursor %= 3
+                    if event.key == pygame.K_RETURN:
+                        if self.playGameMenu.cursor == 0:
+                            self.runningNewGameMenu = True
+                        elif self.playGameMenu.cursor == 1:
+                            self.runningContinueGameMenu = True
+                        elif self.playGameMenu.cursor == 2:
+                            self.runningMainMenu = True
+                        self.runningPlayGameMenu = False
+            elif self.runningOptionsMenu:
+                pass
+            
+                        
+                        
         
     def update(self):
         if self.countTicks % (FPS * 1000 // self.mainMenu.FPS):
-            self.mainMenu.update()
+            if self.runningMainMenu:
+                self.mainMenu.update()
+            if self.runningPlayGameMenu:
+                self.playGameMenu.update()
         
     
     def display(self):
         self.screen.fill(BLACK)
         if self.runningMainMenu:
             self.mainMenu.draw(self.screen)
+        if self.runningPlayGameMenu:
+            self.playGameMenu.draw(self.screen)
         pygame.display.update()
--- code/menu.py
+++ code/menu.py
@@ -1,16 +1,22 @@
 import pygame
 
-MAIN_MENU_FPS = 40
 
-###########  FONT  #############################################################
+###########  FPS  ############################################################################
+MENU_FPS = 40
+
+
+###########  FONT  ############################################################################
 pygame.font.init()
 SIMPLE_SNAKE_FONT = pygame.font.SysFont('algerian', 96)
 TITLE_FONT = pygame.font.SysFont('castellar', 48)
 
-###########  COLOR  ############################################################
+
+###########  COLOR  ###########################################################################
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+
+###########  CLASS BUTTON  ####################################################################
 class Button:
     def __init__(self, text, font, x, y):
         self.text = font.render(text, True, WHITE, None)
@@ -34,16 +40,16 @@ class Button:
     
     def draw(self, parentSurface):
         parentSurface.blit(self.text, self.textRect)
+  
         
-    
-    
-
+###########  CLASS MAIN MENU  #################################################################
 class MainMenu:
     def __init__(self, x, y, width, height):
         self.surface = pygame.Surface((width, height))
         self.surfaceRect = self.surface.get_rect()
         self.surfaceRect.center = (x, y)
-        self.FPS = MAIN_MENU_FPS
+        self.FPS = MENU_FPS
+        self.cursor = 0
         
         self.titleSimpleSnake = Button("SIMPLE SNAKE", SIMPLE_SNAKE_FONT, width//2, height//6)
         self.titleSimpleSnake.draw(self.surface)
@@ -54,8 +60,7 @@ class MainMenu:
         self.titleOptions = Button("OPTIONS", TITLE_FONT, width//2, height*7//12)
         self.titleOptions.draw(self.surface)
         
-        self.cursor = 0
-    
+        
     def update(self):
         if self.cursor == 0:
             self.titlePlayGame.isChosen = True
@@ -73,9 +78,56 @@ class MainMenu:
         self.titleOptions.update('OPTIONS', TITLE_FONT)
         self.titleOptions.draw(self.surface)
         
-        
-
     def draw(self, parentSurface):
         parentSurface.blit(self.surface, self.surfaceRect)
+        
+
+###########  CLASS PLAY GAME MENU  ############################################################
+class PlayGameMenu:
+    def __init__(self, x, y, width, height):
+        self.surface = pygame.Surface((width, height))
+        self.surfaceRect = self.surface.get_rect()
+        self.surfaceRect.center = (x, y)
+        self.FPS = MENU_FPS
+        self.cursor = 0
+        
+        self.titleNewGame = Button("NEW GAME", TITLE_FONT, width//2, height*4//12)
+        self.titleNewGame.draw(self.surface)
+        
+        self.titleContinueGame = Button("CONTINUE GAME", TITLE_FONT, width//2, height*6//12)
+        self.titleContinueGame.draw(self.surface)
+        
+        self.titleBack = Button("BACK", TITLE_FONT, width//2, height*8//12)
+        self.titleBack.draw(self.surface)
+        
+    
+    def update(self):
+        if self.cursor == 0:
+            self.titleNewGame.isChosen = True
+        else:
+            self.titleNewGame.isChosen = False
+        if self.cursor == 1:
+            self.titleContinueGame.isChosen = True
+        else:
+            self.titleContinueGame.isChosen = False
+        if self.cursor == 2:
+            self.titleBack.isChosen = True
+        else:
+            self.titleBack.isChosen = False  
+            
+        self.titleNewGame.update('NEW GAME', TITLE_FONT)
+        self.titleNewGame.draw(self.surface)
+        
+        self.titleContinueGame.update('CONTINUE GAME', TITLE_FONT)
+        self.titleContinueGame.draw(self.surface)
+        
+        self.titleBack.update('BACK', TITLE_FONT)
+        self.titleBack.draw(self.surface)
+        
+    def draw(self, parentSurface):
+        parentSurface.blit(self.surface, self.surfaceRect)
+        
+        
+        
         
         
